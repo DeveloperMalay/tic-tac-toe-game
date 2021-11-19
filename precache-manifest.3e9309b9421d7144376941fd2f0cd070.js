a6696cdec5e629eda07668ccc2c95308e694583b
Malay <malay13pandit@gmail.com>
2021-11-19 22:33:00 +0530

    Updates

---
+++ precache-manifest.3e9309b9421d7144376941fd2f0cd070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0a4bfc09300ddfa053c3a611545fc6",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "931881fdf2d0b65690be",
+    "url": "/tic-tac-toe-game/static/css/main.b54ce5b3.chunk.css"
+  },
+  {
+    "revision": "676f263585fb6d26a853",
+    "url": "/tic-tac-toe-game/static/js/2.abcf3c2d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/tic-tac-toe-game/static/js/2.abcf3c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e5b98db29be1c71972",
+    "url": "/tic-tac-toe-game/static/js/3.9dae1fd8.chunk.js"
+  },
+  {
+    "revision": "931881fdf2d0b65690be",
+    "url": "/tic-tac-toe-game/static/js/main.c84cbf54.chunk.js"
+  },
+  {
+    "revision": "317c8d4105683e27140a",
+    "url": "/tic-tac-toe-game/static/js/runtime-main.5970aee5.js"
+  }
+]);
